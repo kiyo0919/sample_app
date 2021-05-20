cad11a959a3ddd824445264889531473d693d304
Ubuntu <kakinoki0919@gmail.com>
2021-05-20 09:56:08 +0000

    Implement advanced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -72,5 +72,9 @@ test "password should have a minimum length" do
   assert_not @user.valid?
 end
 
+test "authenticated? should return false for a user with nil digest" do
+  assert_not @user.authenticated?('')
+end
+
 
 end
